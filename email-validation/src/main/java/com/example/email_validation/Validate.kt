7a00158dd3082d4891a50d17bc9d03082c51e7f0
sreeramappiness <sreeram@appinessworld.com>
2020-01-09 11:32:13 +0530

    Made function as static

--- email-validation/src/main/java/com/example/email_validation/Validate.kt
+++ email-validation/src/main/java/com/example/email_validation/Validate.kt
@@ -1,19 +1,23 @@
 package com.example.email_validation
 
 class Validate{
-    lateinit var emailPattern: String
-    fun validateEmail(email: String): Boolean{
-        emailPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.+[a-z]+"
-        return email.trim { it <= ' ' }.matches(emailPattern.toRegex())
-    }
+    companion object {
+        lateinit var emailPattern: String
+        fun validateEmail(email: String): Boolean {
+            emailPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.+[a-z]+"
+            return email.trim { it <= ' ' }.matches(emailPattern.toRegex())
+        }
 
-    lateinit var passwordPattern: String
-    fun validatePassword(password: String): Boolean{
-        passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{8,}\$"
-        return password.trim { it <= ' ' }.matches(passwordPattern.toRegex())
-    }
+        lateinit var passwordPattern: String
+        fun validatePassword(password: String): Boolean {
+            passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{8,}\$"
+            return password.trim { it <= ' ' }.matches(passwordPattern.toRegex())
+        }
 
-    fun validateEmailandPassword(email: String, password: String):Boolean{
-        return (email.trim { it <= ' ' }.matches(emailPattern.toRegex()) && password.trim { it <= ' ' }.matches(passwordPattern.toRegex()))
+        fun validateEmailandPassword(email: String, password: String): Boolean {
+            return (email.trim { it <= ' ' }.matches(emailPattern.toRegex()) && password.trim { it <= ' ' }.matches(
+                passwordPattern.toRegex()
+            ))
+        }
     }
 }
