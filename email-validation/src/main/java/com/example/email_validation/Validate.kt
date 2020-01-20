8d0ee385c6716972410ac5f6b4e1df7f87ebe6ab
sreeramappiness <sreeram@appinessworld.com>
2020-01-20 14:48:54 +0530

    Commit

--- email-validation/src/main/java/com/example/email_validation/Validate.kt
+++ email-validation/src/main/java/com/example/email_validation/Validate.kt
@@ -1,26 +1,40 @@
 package com.example.email_validation
 
 class Validate{
-
+    companion object {
         fun validateEmail(email: String): Boolean {
-            var emailPattern: String?
-            emailPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.+[a-z]+"
+            var emailPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.+[a-z]+"
             return email.trim { it <= ' ' }.matches(emailPattern.toRegex())
         }
 
         fun validatePassword(password: String): Boolean {
-            var passwordPattern: String?
-            passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{8,}\$"
+            var passwordPattern =
+                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{8,}\$"
             return password.trim { it <= ' ' }.matches(passwordPattern.toRegex())
         }
 
         fun validateEmailandPassword(email: String, password: String): Boolean {
-            var emailPattern: String?
-            var passwordPattern: String?
-            emailPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.+[a-z]+"
-            passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{8,}\$"
+            var emailPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.+[a-z]+"
+            var passwordPattern =
+                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{8,}\$"
             return (email.trim { it <= ' ' }.matches(emailPattern.toRegex()) && password.trim { it <= ' ' }.matches(
                 passwordPattern.toRegex()
             ))
+        }
+
+        fun validatePhoneNumberIN(phoneNumber: String): Boolean {
+            var phoneNumberPattern = "^[6-9]\\d{9}\$"
+            return (phoneNumber.trim { it <= ' ' }.matches(phoneNumberPattern.toRegex()))
+        }
+
+        fun validatePhoneNumberUS(phoneNumber: String): Boolean {
+            var phoneNumberPatternUS = "^(\\([0-9]{3}\\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}\$"
+            return (phoneNumber.trim { it <= ' ' }.matches(phoneNumberPatternUS.toRegex()))
+        }
+
+        fun validateFullName(fullName: String): Boolean {
+            var fullNamePatter = "^[\\p{L} .'-]+$"
+            return (fullName.trim { it <= ' ' }.matches(fullNamePatter.toRegex()))
+        }
     }
 }
